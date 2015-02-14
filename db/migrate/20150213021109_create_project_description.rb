2373c3e95480909c336ded7b997c989682413f98
Mike Joyce <mikejoyce19@gmail.com>
2015-02-14 12:34:35 -0600

    Revert "Created Migration for Project Desc"
    This reverts commit f5c619bf718b979d1ac9d9598c6458f6bb2becf5.

--- db/migrate/20150213021109_create_project_description.rb
+++
@@ -1,9 +0,0 @@
-class CreateProjectDescription < ActiveRecord::Migration
-  def change
-    create_table :project_descriptions do |t|
-    	t.text :text_body
-    	t.integer :sort_order
-    	t.integer :description_type
-    end
-  end
-end
