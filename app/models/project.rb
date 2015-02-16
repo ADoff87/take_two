be7dc7f3923441a15467eb4abc2ddc655099faea
mcMickJuice <mikejoyce19@gmail.com>
2015-02-15 20:30:12 -0600

    Updated enums
    Update projects enums

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
 	has_many :project_items
-	enum project_type: {drawing: 0, painting: 1, illustrated: 2, finger_paint: 3}
+	enum project_type: {drawing: 0, painting: 1, vector: 2}
 end
