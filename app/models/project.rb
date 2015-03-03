bc8904bac2f615ab0472e4821c7e83c96be50201
mcMickJuice <mikejoyce19@gmail.com>
2015-03-02 22:28:54 -0600

    updated photo urls of seed values
    added helpers for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,12 @@ class Project < ActiveRecord::Base
 	enum project_type: {Drawing: 0, Painting: 1, Vector: 2}
 
 	validates :project_title, presence: true
+
+	def thumbnail_image
+		project_items.where(is_thumb_image: true).first.image_url
+	end
+
+	def project_view_items
+		project_items.where(is_thumb_image: false)
+	end
 end
--- app/models/project_item.rb
+++ app/models/project_item.rb
@@ -1,14 +1,4 @@
 class ProjectItem < ActiveRecord::Base
 	belongs_to :project
-	@@image_path = '../images/'
 
-	def get_thumb_image_url
-		suffix = "_thumb" if is_thumb_image
-		url = "#{@@image_path}#{image_name}#{suffix}.jpg"
-
-	end
-
-	def get_image_url
-		url = "#{@@image_path}#{image_name}.jpg"
-	end
 end
